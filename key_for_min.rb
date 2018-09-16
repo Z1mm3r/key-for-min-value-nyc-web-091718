f1824fe83acd0e950556e6e780370a5bda630fb1
Casey Zimmer <czimmer@ramapo.edu>
2018-09-16 20:39:37 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,19 @@
 
 def key_for_min_value(name_hash)
 
+  smallest_value = nil
+  
+  name_hash.each do |name,value|
+    
+    if(smallest_value == nil)
+      smallest_value = name
+    else
+      if(name_hash[smallest_value] > value)
+        smallest_value = name
+      end
+    end
+  end
+  
+  smallest_value
+
 end
