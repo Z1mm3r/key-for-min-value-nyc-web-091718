7f1a5d25a109d6d1c8e541da4627a3c5d44910b7
Casey Zimmer <czimmer@ramapo.edu>
2018-09-16 20:39:01 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,17 @@
 
 def key_for_min_value(name_hash)
 
+  smallest_value = nil
+  
+  name_hash.each do |name,value|
+    
+    if(smallest_value == nil)
+      smallest_value = name
+    else
+      if(name_hash[smallest_value] > value)
+        smallest_value = name
+  end
+  
   smallest_value
-  name_hash.each do |name
 
 end
